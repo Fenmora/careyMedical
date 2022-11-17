408194b172ac73cacff21191b9c131a45fcfee52
fmora <fmora@cmnhospitals.org>
2022-11-17 16:36:13 -0600

    appointmentPage.js

--- cypress/e2e/firstCase.cy.js
+++ cypress/e2e/firstCase.cy.js
@@ -1,5 +1,7 @@
 const loginPage = require("../pages/loginPage");
 const adminPage = require("../pages/adminPage");
+const patientPage = require("../pages/patientPage");
+const appointmentPage = require("../pages/appointmentPage");
 
 describe("my login application", () => {
   it("should login to carey medical app", () => {
@@ -12,5 +14,20 @@ describe("my login application", () => {
 describe("my agenda application", () => {
   it("should select date from agenda", () => {
     adminPage.openAgenda();
+    patientPage.pagePatientTitle.should("have.text", "Crear Cita");
   });
+
+  it("validate if modal create new patient works", () => {
+    patientPage.openModal();
+    patientPage.isModalPatientVisible();
+  });
+
+  // ? This test is not working issues with conection
+  it("should allow to create a new patient", () => {
+    patientPage.createNewClient();
+  });
+
+  // it("should allow to create a new medical appointment ", () => {
+  //   appointmentPage.createNewAppointment();
+  // });
 });
